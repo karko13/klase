e388781446d89650a08d56c92ca463c9c247185a
karko13 <145532713+karko13@users.noreply.github.com>
2023-12-19 11:21:56 +0100

    Add files via upload

---
+++ Neboder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace neboder
+{
+    internal class Neb
+    {
+        double visina = 0;
+        int brKatova = 0;
+        public Neb()
+        {
+            this.Visina = visina;
+            this.BrKatova = brKatova;
+        }
+        public double Visina { get => visina; set => visina = value; }
+        public int BrKatova { get => brKatova; set => brKatova = value; }
+        public Neb(double visina, int brKatova)
+        {
+            this.Visina = visina;
+            this.BrKatova = brKatova;
+        }
+        public override string ToString()
+        {
+            visina += 10;
+            brKatova += 1;
+            double avgKat = visina / brKatova;
+            string str = "Visina: " + visina + "\nBroj katova: " + brKatova + "\nAvg visina kata: " + avgKat + " metara";
+            return str;
+        }
+    }
+}
